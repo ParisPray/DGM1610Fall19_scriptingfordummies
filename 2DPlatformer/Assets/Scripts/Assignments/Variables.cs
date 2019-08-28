63b76395a0e50b554435318b6c1366cd7e6eb812
Paris <paris.pray@gmail.com>
2019-08-28 17:15:41 -0600

    Added 2D Platformer

---
+++ 2DPlatformer/Assets/Scripts/Assignments/Variables.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables : MonoBehaviour
+{
+    string firstName = "Jeff Goldblum";
+    int age = 420;
+    float height = 6.4f;
+    bool married = true;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("First Name: " + firstName);
+        Debug.Log("Age: " + age);
+        Debug.Log("Height: " + height);
+        Debug.Log("Married: " + married);
+    }
+
+
+}
+
