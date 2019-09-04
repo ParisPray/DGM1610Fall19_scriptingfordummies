12d00d996e7d98ed7e193f30b7ed6150387e6828
Paris Pray <paris.pray@gmail.com>
2019-09-04 14:33:43 -0600

    Added Variable 1 (Squirrel Girl)

---
+++ Variable1.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variable1 : MonoBehaviour
+{
+    string firstName = "Squirrel Girl";
+    int age = 20;
+    float height = 5.3f;
+    bool married = false;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("First Name: " + firstName);
+        Debug.Log("Age: " + age);
+        Debug.Log("Height: " + height);
+        Debug.Log("Married: " + married);
+    }
+
+}
