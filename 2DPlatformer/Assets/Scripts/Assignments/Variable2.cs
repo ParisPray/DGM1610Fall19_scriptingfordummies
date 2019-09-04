9f5dd9dc521d17d1dd815b281a69ac656373103d
Paris <paris.pray@gmail.com>
2019-09-04 17:08:43 -0600

    Made changes to Functions and scene

---
+++ 2DPlatformer/Assets/Scripts/Assignments/Variable2.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variable2 : MonoBehaviour
+{
+    string firstName = "Loki";
+    int age = 1054;
+    float height = 6.2f;
+    bool married = false;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("First Name: " + firstName);
+        Debug.Log("Age: " + age);
+        Debug.Log("Height: " + height);
+        Debug.Log("Married: " + married);
+    }
+
+   
+}
