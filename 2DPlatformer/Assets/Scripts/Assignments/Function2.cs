be2310fefcca3c0d359f7b0c9d06fe6855c567e0
Paris Pray <paris.pray@gmail.com>
2019-09-09 15:53:50 -0600

    added Functions

---
+++ 2DPlatformer/Assets/Scripts/Assignments/Function2.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Function2 : MonoBehaviour
+{
+    // string firstName;
+    // string lastName;
+    // int age;
+
+    int num1;
+
+    int num2;
+
+    int total;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // firstName = "Naruto"
+
+        // lastName = "McAnime";
+
+        // age = 12;
+
+        // Customer(firstName, lastName, age);
+        CalcAdd(69, 420);
+    }
+
+    int CalcAdd(int n1, int n2)
+    {
+        Debug.Log("Number 1 =" + n1 + "Number 2 =" + n2);
+        total = n1 + n2;
+        Debug.Log(total);
+
+        return total;
+
+    }
+
+    // void Customer(string fName, string lName, int a)
+    // {
+    //    Debug.Log(fName + " " + lName + " " + a)
+    // }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
