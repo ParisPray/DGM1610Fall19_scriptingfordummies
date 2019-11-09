e42a2bb31fb0a51ab6bb27af525b51f89f6d79f7
Paris Pray <paris.pray@gmail.com>
2019-11-08 22:42:12 -0700

    Completed Challenge 3

--- Prototype 3/Assets/Challenge 3/Scripts/RepeatBackgroundX.cs
+++ Prototype 3/Assets/Challenge 3/Scripts/RepeatBackgroundX.cs
@@ -4,22 +4,29 @@ using UnityEngine;
 
 public class RepeatBackgroundX : MonoBehaviour
 {
+    private bool gameOver;
     private Vector3 startPos;
     private float repeatWidth;
+    private PlayerControllerX playerControllerScript;
+    private float speed;
 
     private void Start()
     {
         startPos = transform.position; // Establish the default starting position 
         repeatWidth = GetComponent<BoxCollider>().size.x / 2; // Set repeat width to half of the background
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerControllerX>();
     }
 
-    private void Update()
+        void Update()
     {
         // If background moves left by its repeat width, move it back to start position
-        if (transform.position.x < startPos.x - repeatWidth)
+        if (transform.position.x < startPos.x - repeatWidth && playerControllerScript.gameOver == false) 
         {
             transform.position = startPos;
         }
+
+
+
     }
 
  
