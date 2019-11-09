1c48fb3866afc659ff0675d8432ab83e92af87bc
Paris Pray <paris.pray@gmail.com>
2019-11-08 22:41:21 -0700

    Added Prefabs to Personal Project

--- Paris Pray- Personal Project/Assets/Scripts/SpawnManager.cs
+++ Paris Pray- Personal Project/Assets/Scripts/SpawnManager.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    public GameObject[] treasurePrefabs;
+    private float spawnRangeX = 30;
+    private float spawnPosZ = 20;
+    private float spawnPosY = 20;
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomTreasure", startDelay, spawnInterval);
+
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+      
+    }
+    void SpawnRandomTreasure()
+    {
+        int treasureIndex = Random.Range(0, treasurePrefabs.Length);
+
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 20, spawnPosY);
+
+        Instantiate(treasurePrefabs[treasureIndex], spawnPos, treasurePrefabs[treasureIndex].transform.rotation);
     }
 }
