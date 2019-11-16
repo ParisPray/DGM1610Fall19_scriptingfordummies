5d2f427c06bdfa8f4d679be7e777e16848f1bb33
Paris Pray <paris.pray@gmail.com>
2019-11-15 23:45:17 -0700

    Updated Prototype 4

--- Prototype 4/Assets/Course Library/Scripts/PlayerController.cs
+++ Prototype 4/Assets/Course Library/Scripts/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour
     public float speed = 5.0f;
     public bool hasPowerup;
     private float powerupStrength = 15.0f;
+    public GameObject powerupIndicator;
 
     // Start is called before the first frame update
     void Start()
@@ -22,13 +23,17 @@ public class PlayerController : MonoBehaviour
     {
         float forwardInput = Input.GetAxis("Vertical");
         playerRb.AddForce(focalPoint.transform.forward * forwardInput * speed);
+
+        powerupIndicator.transform.position = transform.position + new Vector3(0, -0.5f, 0);
     }
     private void OnTriggerEnter(Collider other)
     {
        if (other.CompareTag("Powerup"))
         {
             hasPowerup = true;
-            Destroy(other.gameObject); 
+            powerupIndicator.gameObject.SetActive(true);
+            Destroy(other.gameObject);
+            StartCoroutine(PowerupCountdownRoutine());
         }
     }
     private void OnCollisionEnter(Collision collision)
@@ -46,5 +51,6 @@ public class PlayerController : MonoBehaviour
     {
         yield return new WaitForSeconds(7);
         hasPowerup = false;
+        powerupIndicator.gameObject.SetActive(false);
     }
 }
